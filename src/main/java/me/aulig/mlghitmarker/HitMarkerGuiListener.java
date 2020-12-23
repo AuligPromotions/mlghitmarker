9db30bcbbc435b5ce2970d008610ec366bfd2c12
Aulig <janbunk87493@gmail.com>
2020-12-23 16:28:47 +0100

    Initial commit

---
+++ src/main/java/me/aulig/mlghitmarker/HitMarkerGuiListener.java
@@ -0,0 +1,181 @@
+package me.aulig.mlghitmarker;
+
+import io.netty.util.internal.ThreadLocalRandom;
+import net.labymod.api.LabyModAddon;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.ScaledResolution;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.FloatControl;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class HitMarkerGuiListener {
+
+    private final ResourceLocation markerTexture = new ResourceLocation("marker.png");
+
+    private LabyModAddon addon;
+
+    private int hitMarkerSize;
+    // time to display marker in milliseconds
+    private int hitMarkerLength;
+
+    private long hideAt = 0;
+
+    private float hitMarkerVolume;
+
+    private boolean useMemeSoundEffects;
+
+    private boolean randomize;
+    private int randomOffset = 0;
+
+    private List<Long> lastKills = new ArrayList<Long>();
+
+    HitMarkerGuiListener(LabyModAddon addon, int hitMarkerSize, int hitMarkerVolume, boolean randomize, int hitMarkerLength, boolean useMemeSoundEffects) {
+
+        this.addon = addon;
+        this.hitMarkerSize = hitMarkerSize;
+        setHitMarkerVolume(hitMarkerVolume);
+        this.randomize = randomize;
+        this.hitMarkerLength = hitMarkerLength;
+        this.useMemeSoundEffects = useMemeSoundEffects;
+    }
+
+    public void setHitMarkerLength(int hitMarkerLength) {
+        this.hitMarkerLength = hitMarkerLength;
+    }
+
+    public void setRandomize(boolean randomize) {
+        this.randomize = randomize;
+    }
+
+    public void setHitMarkerVolume(int hitMarkerVolume) {
+        this.hitMarkerVolume = hitMarkerVolume / 100f;
+    }
+
+    public void setHitMarkerSize(int hitMarkerSize) {
+        this.hitMarkerSize = hitMarkerSize;
+    }
+
+    public void setUseMemeSoundEffects(boolean useMemeSoundEffects) {
+        this.useMemeSoundEffects = useMemeSoundEffects;
+    }
+
+    public int getHitMarkerVolume() {
+        return Math.round(hitMarkerVolume * 100);
+    }
+
+    public int getHitMarkerLength() {
+        return hitMarkerLength;
+    }
+
+    public int getHitMarkerSize() {
+        return hitMarkerSize;
+    }
+
+    public boolean shouldRandomize() {
+        return randomize;
+    }
+
+    public boolean shouldUseMemeSoundEffects() {
+        return useMemeSoundEffects;
+    }
+
+    void showHitMarker(final boolean isKill) {
+
+        long currentTime = System.currentTimeMillis();
+
+        boolean isTripleKill = false;
+
+        if (isKill) {
+
+            lastKills.add(currentTime);
+
+            if (lastKills.size() > 3) {
+
+                lastKills.remove(0);
+                isTripleKill = lastKills.get(2) - lastKills.get(0) < 7000;
+                System.out.println(lastKills.get(2) - lastKills.get(0));
+
+                if (isTripleKill) {
+
+                    lastKills.clear();
+                }
+            }
+        }
+
+
+        hideAt = currentTime + hitMarkerLength;
+
+        if (randomize) {
+
+            randomOffset = ThreadLocalRandom.current().nextInt(-(hitMarkerSize / 2), hitMarkerSize / 2);
+        }
+
+        try {
+
+            final boolean finalIsTripleKill = isTripleKill;
+            Thread t = new Thread(new Runnable() {
+                @Override
+                public void run() {
+
+                    try {
+
+                        URL resource;
+
+                        if (useMemeSoundEffects && finalIsTripleKill) {
+                            resource = getClass().getResource("/assets/minecraft/triple.wav");
+                        } else if (useMemeSoundEffects && isKill && ThreadLocalRandom.current().nextInt(0, 20) == 0) {
+                            resource = getClass().getResource("/assets/minecraft/cenal.wav");
+                        } else {
+                            resource = getClass().getResource("/assets/minecraft/hit.wav");
+                        }
+
+                        AudioInputStream audioIn = AudioSystem.getAudioInputStream(resource);
+                        Clip clip = AudioSystem.getClip();
+                        clip.open(audioIn);
+                        FloatControl fc = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+                        float range = fc.getMaximum() - fc.getMinimum();
+                        float gain = (range * hitMarkerVolume) + fc.getMinimum();
+                        fc.setValue(gain);
+                        clip.start();
+                    } catch (Exception e) {
+
+                        System.out.println(e.getMessage());
+                    }
+                }
+            });
+            t.start();
+
+        } catch (Exception e) {
+
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @SubscribeEvent
+    public void onTick(TickEvent.RenderTickEvent event) {
+
+        if (System.currentTimeMillis() < hideAt) {
+            Minecraft.getMinecraft().getTextureManager().bindTexture(markerTexture);
+            ScaledResolution scaledResolution = addon.getApi().getDrawUtils().getScaledResolution();
+
+            double posX = scaledResolution.getScaledWidth() / 2d - (hitMarkerSize / 2d);
+            double posY = scaledResolution.getScaledHeight() / 2d - (hitMarkerSize / 2d);
+
+            if (randomize) {
+
+                posX += randomOffset;
+                posY += randomOffset;
+            }
+
+            addon.getApi().getDrawUtils().drawTexture(posX, posY, 512d / 2d, 512d / 2d, hitMarkerSize, hitMarkerSize, 1);
+        }
+    }
+}
---
+++ src/main/java/me/aulig/mlghitmarker/Main.java
@@ -0,0 +1,153 @@
+package me.aulig.mlghitmarker;
+
+import net.labymod.api.LabyModAddon;
+import net.labymod.settings.elements.BooleanElement;
+import net.labymod.settings.elements.ControlElement;
+import net.labymod.settings.elements.SettingsElement;
+import net.labymod.settings.elements.SliderElement;
+import net.labymod.utils.Consumer;
+import net.labymod.utils.Material;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+
+import java.util.List;
+
+public class Main extends LabyModAddon {
+
+    HitMarkerGuiListener guiListener;
+
+    static final int HIT_MARKER_DEFAULT_SIZE = 25;
+    static final int HIT_MARKER_DEFAULT_LENGTH_MS = 200;
+    static final int HIT_MARKER_DEFAULT_VOLUME = 70;
+    static final boolean HIT_MARKER_DEFAULT_RANDOMIZE = true;
+    static final boolean USE_MEME_SOUND_EFFECTS_DEFAULT = true;
+
+    @Override
+    public void onEnable() {
+
+        guiListener = new HitMarkerGuiListener(this, HIT_MARKER_DEFAULT_SIZE, HIT_MARKER_DEFAULT_VOLUME, HIT_MARKER_DEFAULT_RANDOMIZE, HIT_MARKER_DEFAULT_LENGTH_MS, USE_MEME_SOUND_EFFECTS_DEFAULT);
+
+        getApi().getEventManager().registerOnAttack(new Consumer<Entity>() {
+            @Override
+            public void accept(Entity entity) {
+
+                boolean isKill = false;
+
+                if (entity instanceof EntityLivingBase) {
+
+                    isKill = ((EntityLivingBase) entity).getHealth() == 0;
+                }
+
+                guiListener.showHitMarker(isKill);
+            }
+        });
+
+        getApi().registerForgeListener(guiListener);
+    }
+
+    @Override
+    public void loadConfig() {
+
+        if (getConfig().has("hitMarkerSize")) {
+
+            guiListener.setHitMarkerSize(getConfig().get("hitMarkerSize").getAsInt());
+        }
+
+        if (getConfig().has("hitMarkerVolume")) {
+
+            guiListener.setHitMarkerVolume(getConfig().get("hitMarkerVolume").getAsInt());
+        }
+
+        if (getConfig().has("hitMarkerLength")) {
+
+            guiListener.setHitMarkerLength(getConfig().get("hitMarkerLength").getAsInt());
+        }
+
+        if (getConfig().has("hitMarkerRandomize")) {
+
+            guiListener.setRandomize(getConfig().get("hitMarkerRandomize").getAsBoolean());
+        }
+
+        if (getConfig().has("memeSoundEffects")) {
+
+            guiListener.setUseMemeSoundEffects(getConfig().get("memeSoundEffects").getAsBoolean());
+        }
+    }
+
+    @Override
+    protected void fillSettings(List<SettingsElement> settingsElementList) {
+
+        SliderElement hitMarkerSizeSlider = new SliderElement("HitMarker Size", this,
+                new ControlElement.IconData(Material.ARROW), "hitMarkerSize", guiListener.getHitMarkerSize());
+
+        hitMarkerSizeSlider.setRange(1, 50);
+
+        hitMarkerSizeSlider.addCallback(new Consumer<Integer>() {
+            @Override
+            public void accept(Integer newHitMarkerSize) {
+                guiListener.setHitMarkerSize(newHitMarkerSize);
+
+            }
+        });
+        settingsElementList.add(hitMarkerSizeSlider);
+
+
+        SliderElement hitMarkerVolumeSlider = new SliderElement("HitMarker Volume", this,
+                new ControlElement.IconData(Material.NOTE_BLOCK), "hitMarkerVolume", guiListener.getHitMarkerVolume());
+
+        hitMarkerVolumeSlider.setRange(1, 100);
+
+        hitMarkerVolumeSlider.addCallback(new Consumer<Integer>() {
+            @Override
+            public void accept(Integer newHitMarkerVolume) {
+                guiListener.setHitMarkerVolume(newHitMarkerVolume);
+            }
+        });
+
+        settingsElementList.add(hitMarkerVolumeSlider);
+
+
+        SliderElement hitMarkerLengthSlider = new SliderElement("HitMarker Display Time", this,
+                new ControlElement.IconData(Material.WATCH), "hitMarkerLength", guiListener.getHitMarkerLength());
+
+        hitMarkerLengthSlider.setRange(10, 1000);
+        hitMarkerLengthSlider.setSteps(10);
+
+        hitMarkerLengthSlider.addCallback(new Consumer<Integer>() {
+            @Override
+            public void accept(Integer newHitMarkerVolume) {
+                guiListener.setHitMarkerVolume(newHitMarkerVolume);
+            }
+        });
+
+        settingsElementList.add(hitMarkerLengthSlider);
+
+
+        BooleanElement randomizeToggle = new BooleanElement("Randomize HitMarker Position", this,
+                new ControlElement.IconData(Material.COMMAND), "hitMarkerRandomize", guiListener.shouldRandomize());
+
+        randomizeToggle.addCallback(new Consumer<Boolean>() {
+            @Override
+            public void accept(Boolean newRandomize) {
+                guiListener.setRandomize(newRandomize);
+            }
+        });
+
+        settingsElementList.add(randomizeToggle);
+
+
+        BooleanElement memeSoundEffectsToggle = new BooleanElement("Meme Sound Effects", this,
+                new ControlElement.IconData(Material.COMMAND), "memeSoundEffects", guiListener.shouldUseMemeSoundEffects());
+
+        memeSoundEffectsToggle.addCallback(new Consumer<Boolean>() {
+            @Override
+            public void accept(Boolean newUseMemeSoundEffects) {
+                guiListener.setUseMemeSoundEffects(newUseMemeSoundEffects);
+            }
+        });
+
+        settingsElementList.add(memeSoundEffectsToggle);
+    }
+
+
+}
